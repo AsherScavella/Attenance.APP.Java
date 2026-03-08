73fe6381812c83f96481e9633cceef67e9392bc9
Asher Scavella <ashersnow242@gmail.com>
2026-03-08 08:49:11 -0500

    methods

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(1);
+        System.out.println(s1);
+
+        Student s2 = new Student(1, "Debbie");
+        System.out.println(s2);
+
+
+    }
+}
+
